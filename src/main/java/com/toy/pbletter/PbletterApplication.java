555345a2f6086589585ee4378ce4e5c558637bdc
JongChan <kjc3590@songareeit.com>
2021-06-07 15:56:54 +0900

    [Letter] Letter init

---
+++ src/main/java/com/toy/pbletter/PbletterApplication.java
@@ -0,0 +1,17 @@
+package com.toy.pbletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@RefreshScope
+@EnableFeignClients
+public class PbletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbletterApplication.class, args);
+    }
+
+}
