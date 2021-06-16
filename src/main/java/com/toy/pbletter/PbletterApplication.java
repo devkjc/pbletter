c8b32aa4ac30d870c4af8478cb5ce6e991facbd1
JongChan <kjc3590@songareeit.com>
2021-06-16 18:50:32 +0900

    [Config] Zuul 세팅

--- src/main/java/com/toy/pbletter/PbletterApplication.java
+++ src/main/java/com/toy/pbletter/PbletterApplication.java
@@ -2,12 +2,14 @@ package com.toy.pbletter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @RefreshScope
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PbletterApplication {
 
     public static void main(String[] args) {
