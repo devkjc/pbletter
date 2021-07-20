755fbb8fbac14ca51e02de6521a9068212d6d4d3
JongChan <kjc3590@gmail.com>
2021-07-20 22:55:56 +0900

    [Init] project security

--- src/main/java/com/toy/pbletter/config/security/ResourceServerConfiguration.java
+++ src/main/java/com/toy/pbletter/config/security/ResourceServerConfiguration.java
@@ -2,6 +2,7 @@ package com.toy.pbletter.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
@@ -9,6 +10,12 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests()
+                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/configuration/**", "/webjars/**", "/api-docs").permitAll()
+                .antMatchers("/oauth/**", "/csrf").permitAll()
+//                .antMatchers("/", "/hello","/profile","/api/v1/user/test").permitAll()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
